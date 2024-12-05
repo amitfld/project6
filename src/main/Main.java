cb8e242a0623233dacdb6329e0f6dc5aeb59e9e5
Amit Feldman <amit.feldman@post.runi.ac.il>
2024-12-05 13:42:44 +0200

    Ben sheli yakar

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class Main {
+    // bennnnnnn sheliiiiiiiiiiiiiii
 
     public static void main(String[] args) throws IOException {
         if (args.length != 1){
